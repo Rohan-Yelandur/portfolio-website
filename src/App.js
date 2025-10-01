fc00501648d5517a8fa986f765014913790e1190
Rohan-Yelandur <ry7games@gmail.com>
2025-09-30 23:20:06 -0500

    Origins

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
 import './App.css';
+import {Hero} from './components/index';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Footer />
+      <Hero/>
     </div>
   );
 }
